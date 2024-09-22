ffb236be11518e33bbfb0d1374e567399f9bd37a
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-09-22 14:29:06 +0700

    Change app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from "./game";
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from "./game.js";
 
 const game = new Game();
 game.start();
